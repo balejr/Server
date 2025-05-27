d7a3f770d82af867cd5e458509815c8b60d2cfe7
Bonifacio Ale <b.ale.jr.16@gmail.com>
2025-05-26 22:10:01 -0700

    Exercise Existence

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -303,17 +303,23 @@ router.get('/exerciseexistence/date/:date', authenticateToken, async (req, res) 
       const result = await pool.request()
         .input('userId', userId)
         .input('date', date)
-        .query(`SELECT ee.*, e.ExerciseName 
-              FROM dbo.ExerciseExistence ee
-              INNER JOIN (SELECT UserId, MAX([Date]) as [Date] 
-                  FROM dbo.ExerciseExistence 
-                  WHERE UserID = @userid
-                  AND CONVERT(date, [Date]) = @date
-                  GROUP BY UserId) eex
-              ON ee.UserId = eex.UserId
-              AND ee.[Date] = eex.[Date]
-              LEFT JOIN dbo.[Exercise] e 
-              ON ee.ExerciseId = e.ExerciseId `)
+        .query(`SELECT ex.* 
+                FROM dbo.ExerciseExistence ex
+                INNER JOIN 
+                (
+                SELECT MAX(ee.ExerciseExistenceID) as ExerciseExistenceID, ee.UserId, ee.ExerciseID
+                FROM dbo.ExerciseExistence ee
+                INNER JOIN (SELECT UserId, MAX(FORMAT([Date], 'yyyy-MM-dd')) as [Date] 
+                    FROM dbo.ExerciseExistence 
+                    WHERE UserID = @userId
+                    GROUP BY UserId) eex
+                ON ee.UserId = eex.UserId
+                AND FORMAT(ee.[Date], 'yyyy-MM-dd') = eex.[Date]
+                GROUP BY ee.UserId, ee.ExerciseID
+                ) ey
+                on ex.ExerciseExistenceID = ey.ExerciseExistenceID
+                LEFT JOIN dbo.[Exercise] e 
+                ON ex.ExerciseId = e.ExerciseId `)
         // .query('SELECT ee.*, e.ExerciseName FROM dbo.ExerciseExistence ee LEFT JOIN dbo.[Exercise] e ON ee.ExerciseId = e.ExerciseId WHERE ee.UserID = @userId AND CONVERT(date, ee.Date) = @date');
       res.status(200).json(result.recordset);
     } catch (err) {
