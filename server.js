b843b0ee4c530ec12381c038c70f98f6d29208ca
Bonifacio Ale <b.ale.jr.16@gmail.com>
2025-09-16 15:38:46 -0700

    updated

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-app.listen(PORT, "0.0.0.0", () => {
-  // console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+}); 
