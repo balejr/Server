83a8e840e37b4a11fa9e5ccd57d504d2b5701184
Bonifacio Ale <b.ale.jr.16@gmail.com>
2025-09-16 02:48:24 -0700

    updated

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/api/usage", usageRoutes);
 
 // Root route for health check
 app.get("/", (req, res) => {
-  res.send("ApogeeHnP Backend is running!");
+  res.send("ApogeeHnP backend is running!");
 });
 
 // Start server
