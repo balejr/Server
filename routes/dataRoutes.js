5930d33d1a9563901f33669545092dc4abe39ad2
mjjr19 <mubasheer.joban19@gmail.com>
2025-11-02 22:31:30 -0800

    added payments_id

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -994,21 +994,22 @@ router.get('/exercises/history/:userId', async (req, res) => {
 router.post('/payments', authenticateToken, async (req, res) => {
     const userId = req.user.userId;
     const status = "pending";
-    const { plan, amount, currency, paymentMethod, created_date} = req.body;
+    const { payments_id, plan, amount, currency, paymentMethod, created_date} = req.body;
 
     try {
       const pool = getPool();
       await pool.request()
+        .input('payments_id', payments_id)
         .input('userId', userId)
         .input('plan', plan)
         .input('amount', amount)
         .input('currency', currency)
         .input('paymentMethod', paymentMethod)
-        .input('createdDate', created_date)
+        .input('created_date', created_date)
         .input('status', status)
         .query(`
-          INSERT INTO dbo.payments (plan, amount, currency, paymentMethod, status)
-          VALUES (@plan, @amount, @currency, @paymentMethod, @created_date, @status)
+          INSERT INTO dbo.payments (payments_id, plan, amount, currency, paymentMethod, status)
+          VALUES (@payments_id, @plan, @amount, @currency, @paymentMethod, @created_date, @status)
         `);
       res.status(200).json({ message: 'Payment added successfully' });
     } catch (err) {
