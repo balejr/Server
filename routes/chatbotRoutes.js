891fa0e824a550c0c4dfac1c5dacf398b4a7143e
mjjr19 <mubasheer.joban19@gmail.com>
2025-11-21 15:36:17 -0800

    Fix Google Generative AI import statement

--- routes/chatbotRoutes.js
+++ routes/chatbotRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const { getPool } = require("../config/db");
 const { authenticateToken } = require("../middleware/authMiddleware");
-const { GoogleGenAI, Type } = require("@google/genai");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const { checkUsageLimit, incrementUsage } = require("./usageRoutes");
 const { generatePlanId, saveWorkoutPlan } = require("./workoutRoutes");
 
