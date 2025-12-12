5791f62d06edf38d2e3ed8adcf6fb2bcc412234c
Paul <pauladriansese@gmail.com>
2025-12-12 12:17:35 -0800

    Fixed code

--- services/ouraService.js
+++ services/ouraService.js
@@ -4,7 +4,7 @@ module.exports = {
   exchangeCodeForToken: async (code) => {
     const res = await axios.post("https://api.ouraring.com/oauth/token", {
       grant_type: "authorization_code",
-      code,
+      code: code,
       redirect_uri: process.env.OURA_REDIRECT_URI,
       client_id: process.env.OURA_CLIENT_ID,
       client_secret: process.env.OURA_CLIENT_SECRET,
