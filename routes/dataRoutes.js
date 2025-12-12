aba84f462a6f9ca35d7cc3295074f7103a971cca
Paul <pauladriansese@gmail.com>
2025-12-12 14:39:28 -0800

    Fixed Users to FROM OuraTokens

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -4152,7 +4152,7 @@ router.post('/oura/sync', authenticateToken, async (req, res) => {
       .input('userId', userId)
       .query(`
         SELECT OuraAccessToken, OuraRefreshToken
-        FROM Users
+        FROM OuraTokens
         WHERE UserID = 'userId'
       `);
 
