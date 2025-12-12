2af7f5c2af40eee7f9c3fc5a93dcac7db8fcc1b3
Paul <pauladriansese@gmail.com>
2025-12-12 09:34:46 -0800

    Added callback

---
+++ services/ouraService.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+module.exports = {
+  exchangeCodeForToken: async (code) => {
+    const res = await axios.post("https://api.ouraring.com/oauth/token", {
+      grant_type: "authorization_code",
+      code,
+      redirect_uri: process.env.OURA_REDIRECT_URI,
+      client_id: process.env.OURA_CLIENT_ID,
+      client_secret: process.env.OURA_CLIENT_SECRET,
+    });
+    return res.data;
+  },
+};
